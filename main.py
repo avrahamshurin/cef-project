83fd0268fe009a512d78d0d0206aa17e7aed3639
avrahamshurin <avraham14@gmail.com>
2022-07-17 18:39:24 +0300

    excel and removed index column

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from io import BytesIO
 from cef_data_manager import CefDataManager
+from datetime import datetime
 import streamlit as st
+import pandas as pd
 
+def get_file_name(extension):
+    return "{}{}.{}".format('screener_summary', datetime.now().strftime('%y%m%d'), extension)
 
 cef_data_manager = CefDataManager()
 df = cef_data_manager.get_data()
 
-csv = df.to_csv()
+in_memory_fp = BytesIO()
+with (pd.ExcelWriter(in_memory_fp, engine='xlsxwriter')) as writer:
+    df.to_excel(writer, index=False)
+
+excel = in_memory_fp.getvalue()
+csv = df.to_csv(index=False)
 
 st.dataframe(df)
-st.download_button('Press to Download', csv, 'cef.csv')
+st.download_button('Download as CSV', csv, get_file_name('csv'))
+st.download_button('Download as Excel', excel, get_file_name('xlsx'))
